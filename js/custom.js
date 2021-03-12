f39eddde11aa557ea3e0df135a817e2eff13b0c6
Damini Sherathiya <daminisherathiya@gmail.com>
2021-03-12 21:57:22 +0530

    Fixes.

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,10 @@
 $(function() {
 "use strict";
+
     var sidebar_toggler = document.querySelectorAll(".sidebar-toggler");
     var left_sidebar = document.querySelector(".left-sidebar");
 
+    // small screen in tablet view
     var width = (window.innerWidth > 0) ? window.innerWidth : this.screen.width;
     if (width<992 && width>767) {
         left_sidebar.classList.add("small-sidebar");
@@ -11,15 +13,13 @@ $(function() {
         left_sidebar.classList.remove("small-sidebar");
     }
 
-
+    // minimize sidebar
     sidebar_toggler[0].addEventListener("click", minimize_sidebar);
     sidebar_toggler[1].addEventListener("click", minimize_sidebar);
 
     function minimize_sidebar() {
-        // console.log(left_sidebar)
         left_sidebar.classList.toggle("small-sidebar");
         if (width<576) {
-            // console.log(left_sidebar.style.left)
             if (left_sidebar.style.left === "0px") {
               left_sidebar.style.left = "-265px";
             } else {
@@ -27,6 +27,18 @@ $(function() {
             }
         }
     }
+
+    // when sidebar-link is active
+    // var li_first_sidebar_links = document.querySelectorAll(".sidebar-nav li .sidebar-link.has-arrow");
+    // var sidebar_sublinks_sidebar_link = document.querySelectorAll(".sidebar-sublink .sidebar-link");
+
+    // li_first_sidebar_links.forEach(element => {
+    //     element.addEventListener("click", open_sidebar_sublink);
+    // });
+    // function open_sidebar_sublink(event) {
+    //     console.log("hello");
+    // }
+    
 });
 
 // var mq = window.matchMedia( "(max-width: 991px)" );
